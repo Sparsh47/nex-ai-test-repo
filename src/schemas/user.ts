a1f5a70f9f6204488dc34d1dee5f3aaf5371b1cb
Sparsh47 <sparshshandilya123@gmail.com>
2026-05-15 22:34:02 +0530

    again created basic fastify setup for testing

--- src/schemas/user.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const UserPatchSchema = z.object({
-  name: z.string().optional(),
-  email: z.string().email().optional(),
-  role: z.enum(['user', 'admin']).optional()
-});
