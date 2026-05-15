2fc93ae8fb137c87364105d384d44a5361762a80
Sparsh Shandilya <95901042+Sparsh47@users.noreply.github.com>
2026-05-15 22:37:46 +0530

    Create user update schema

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,16 +1,6 @@
 import { z } from 'zod';
 
-export const UserSchema = z.object({
-  id: z.string(),
-  email: z.string().email(),
-  name: z.string()
-});
-
-export type User = z.infer<typeof UserSchema>;
-
-export const UserUpdateSchema = z.object({
+export const userUpdateSchema = z.object({
   name: z.string().optional(),
   email: z.string().email().optional()
 });
-
-export type UserUpdate = z.infer<typeof UserUpdateSchema>;
