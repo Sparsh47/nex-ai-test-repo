4222249d0da4e76b3ecb45aa0290a276dd217d84
Sparsh Shandilya <95901042+Sparsh47@users.noreply.github.com>
2026-05-16 13:35:39 +0530

    Create Zod schema for PATCH validation

--- src/schemas/userPatchSchema.ts
+++ src/schemas/userPatchSchema.ts
@@ -2,6 +2,5 @@ import { z } from 'zod';
 
 export const userPatchSchema = z.object({
   name: z.string().optional(),
-  email: z.string().email().optional(),
-  password: z.string().min(6).optional(),
+  email: z.string().email().optional()
 });
