96d883631afdaa76937ad58049b68b8da2fd02cb
Sparsh Shandilya <95901042+Sparsh47@users.noreply.github.com>
2026-05-16 12:13:03 +0530

    Create user validation schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
-export const userPatchSchema = z.object({
+export const patchUserSchema = z.object({
   name: z.string().optional(),
   email: z.string().email().optional(),
-  bio: z.string().max(500).optional()
+  bio: z.string().optional()
 });
